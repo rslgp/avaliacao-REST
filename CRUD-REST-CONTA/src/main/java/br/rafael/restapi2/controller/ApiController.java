247a599991fd135cf27addb60e51b754b537d380
rslgp <rslgp@cin.ufpe.br>
2023-09-01 17:09:51 -0300

    Update ApiController.java
    AUTO AJUSTAR em caso de valores errados

--- CRUD-REST-CONTA/src/main/java/br/rafael/restapi2/controller/ApiController.java
+++ CRUD-REST-CONTA/src/main/java/br/rafael/restapi2/controller/ApiController.java
@@ -36,6 +36,20 @@ public class ApiController {
     
     @PostMapping("/transacao")
     private void createTransacao(@RequestBody Map<String,String> payload) {
+    	//AUTO AJUSTAR converter sozinho caso venha valor errado
+    	String tipoTransacao = payload.get("tipo_operacao_id");
+		Double valor = Double.parseDouble(payload.get("valor"));
+    	switch(tipoTransacao) {
+    		case "1":;
+    		case "2":
+    		case "3":
+    			if(valor>0) payload.put("valor", (valor * -1)+"");
+    			break;
+    		case "4":
+    			if(valor<0) payload.put("valor", (valor * -1)+"");
+    			break;
+    	}
+    	
     	banco.createTransacao(payload);
     }
     
